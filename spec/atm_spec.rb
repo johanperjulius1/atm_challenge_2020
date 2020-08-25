7020866a30b6b8cc06fe61a11438672267957900
emiliano-ma <emilianomainero@gmail.com>
2020-08-25 18:18:41 +0200

    making the feedback hashes into one line in the atm_spec file

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -20,9 +20,7 @@ describe Atm do
     end
 
     it 'allow withdraw if the account has enough balance.' do
-        expected_output = { 
-            status: true, message: 'success', date: Date.today, amount: 45
-        }
+        expected_output = { status: true, message: 'success', date: Date.today, amount: 45}
         
         expect(subject.withdraw(45, account)).to eq expected_output
     end
