bfbb5f6c7d53ee917d31707e69eb55f990a4fbf6
Connor Roelofsen <connorroelofsen@Connors-MacBook-Air.local>
2020-08-25 11:59:42 +0200

    Adding integration spec, line: 17.

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -8,6 +8,18 @@ describe Atm do
     it 'funds are reduced at withdraw' do
         subject.withdraw 50
         expect(subject.funds).to eq 950
-    
+    end
+
+    let(:account)   {
+        instance_double('Account')
+    }
+
+    before do
+        #Before each test we need to add an attribute of 'balance'
+        #to the 'account' object and set the value to '100'.
+        allow(account).to receive(:balance).and_return(100)
+        #We also need to aloow account to receive the new balance
+        #using the setter method 'balance='
+        allow(account).to recieve(:balance=)
     end
 end
