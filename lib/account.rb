a1cdd66dbeec7b522e89e973dce40b0099f1d5f3
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-08-28 16:50:51 +0200

    I am doing some changes compared to Emilianos latest pushed master file to get the account owner test to pass. It is not passing.

--- lib/account.rb
+++ lib/account.rb
@@ -1,31 +1,33 @@
 require 'date'
 
 class Account
-    attr_accessor :account_status, :owner, :exp_date, :pin_code
-   
+  attr_accessor :account_status, :exp_date, :pin_code, :balance, :owner
 
-    STANDARD_VALIDITY_YEARS = 5
+  STANDARD_VALIDITY_YEARS = 5
 
-    def initialize
-        @account_status = :active
-        @pin_code = rand(1000..9999)
-        @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YEARS).strftime("%m/%y")
-        # set_owner(attrs[:owner])
-    end
+  def initialize(attrs = {})
+    @account_status = :active
+    @pin_code = rand(1000..9999)
+    @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YEARS).strftime('%m/%y')
+    @balance = 0
+    @owner = set_owner(attrs[:owner])
+  end
 
-    def deactivate
-        @account_status = :deactivated
-    end
+  def deactivate
+    @account_status = :deactivated
+  end
 
-    def length_check(pin_code)
-        number_length = Math.log10(pin_code).to_i + 1
-    end
+  def length_check(pin_code)
+    number_length = Math.log10(pin_code).to_i + 1
+  end
 
-    private
-    def set_owner(obj)
-        obj == nil ?  missing_owner : @owner = obj
-    end
-    def missing_owner
-        raise "An Account owner is required"
-    end
+  #private
+
+  def set_owner(obj)
+    obj.nil? ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise RuntimeError, 'An Account owner is required'
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,33 +1,35 @@
 require './lib/account.rb'
-require 'date'
+
+
 
 describe Account do
-    it 'is expected to have :active status on initilization' do
-        expect(subject.account_status).to eq :active
-    end
+  let(:person) {instance_double('Person', name: 'emi')}
+  subject {described_class.new({owner: person})}
 
-    it 'deactivates account using the instance method' do
-        subject.deactivate
-        expect(subject.account_status).to eq :deactivated
-    end
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
 
-    it 'to check if pin code is 4 digits' do
-        expect(subject.length_check(1000)).to eq 4
-    end
+  it 'is expected to have :active status on initilization' do
+    expect(subject.account_status).to eq :active
+  end
 
-    it 'expected to have an expiry date on initialize' do
-        expected_date = Date.today.next_year(5).strftime("%m/%y")
-        expect(subject.exp_date).to eq expected_date
-    end
+  it 'deactivates account using the instance method' do
+    subject.deactivate
+    expect(subject.account_status).to eq :deactivated
+  end
 
-    let(:person) {instance_double('Person', name: 'connor')}
-    subject { described_Class.new({owner: person}) }
+  it 'to check if pin code is 4 digits' do
+    expect(subject.length_check(1000)).to eq 4
+  end
 
-    it 'is expected to have an owner' do
-        expect(subject.owner).to eq person
-    end
-    
-    it 'expected to raise error if no owner is set' do
-        expect { describe_class.new }.to raise_error 'An account owner is required'
-    end
+  it 'expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime('%m/%y')
+    expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is expected to raise error if no owner is set' do
+   #binding.pry
+    expect {described_class.new}.to raise_error 'An account owner is required'
+  end
 end
